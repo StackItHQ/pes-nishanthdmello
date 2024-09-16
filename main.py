11a27b3ee2812ecfeea5f7aaa8b94dedd8249cb5
Nishanth <nishanthdmello2003@gmail.com>
2024-09-16 12:42:59 +0530

    creted database using google sheet name

--- main.py
+++ main.py
@@ -20,3 +20,21 @@ cred = args.cred
 scope = ["https://www.googleapis.com/auth/spreadsheets"]
 creds = Credentials.from_service_account_file(cred, scopes=scope)
 client = gspread.authorize(creds)
+
+
+try:
+    sheet = client.open_by_url(url)
+except:
+    print("Run python3 main.py --url <google sheet url>")
+    sys.exit()
+
+
+try:
+    database = sheet.title
+    query = f"CREATE DATABASE IF NOT EXISTS {database};"
+    sql.execute(query)
+    query = f"USE {database};"
+    sql.execute(query)
+    conn.commit()
+except:
+    print('creation of database failed...')
