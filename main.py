5a02be69d5102dd8572187f5c70167c40331fe8b
Nishanth <nishanthdmello2003@gmail.com>
2024-09-16 12:41:25 +0530

    added imports and google credentials

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print('hello world')
+import sys
+import argparse
+import gspread
+from google.oauth2.service_account import Credentials
+from MySQL_Connection import connect_to_mysql
+from functions import *
+import time
+
+
+conn=connect_to_mysql()
+sql = conn.cursor()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--url", required=True, help="google sheet url")
+parser.add_argument("--cred", required=True, help="credentials json file")
+args = parser.parse_args()
+url = args.url
+cred = args.cred
+scope = ["https://www.googleapis.com/auth/spreadsheets"]
+creds = Credentials.from_service_account_file(cred, scopes=scope)
+client = gspread.authorize(creds)
